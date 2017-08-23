d2144891fb934850458be49f205ae4ad79a9a46b
kamarajanis <kamarajkamarajanis@gmail.com>
2017-08-23 18:01:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one',function(req,res){
+    res.send("This is articlw one");
+});
+app.get('/article-two',function(req,res){
+    res.send("This is articlw two");
+});
+app.get('/article-threee',function(req,res){
+    res.send("This is articlw three");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
